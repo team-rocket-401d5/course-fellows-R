fecb589db16897e650bee29e1891869cdf3dec98
Mohammad-Eshtaiwi <mohd.eshtiwi@gmail.com>
2020-12-11 04:25:35 +0200

    landing page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
+import { Router, Route, Switch } from 'react-router';
 import Register from './components/Register';
 import RegisterProvider from './context/auth';
-import Auth from '../src/context/outhprevent';
+import Auth from './context/outhprevent';
+import Landing from './components/landingPage';
+import Nav from './components/nav';
 
 function App() {
-	return (
-		<RegisterProvider>
-			<Auth />
-			<Register />
-		</RegisterProvider>
-	);
+  return (
+    <RegisterProvider>
+      {/* <Auth />
+			<Register /> */}
+      <Nav />
+      {/* 
+      <Landing /> */}
+      <Switch>
+        <Route exact path="/" component={Landing} />
+        <Route path="/register" component={Register} />
+      </Switch>
+    </RegisterProvider>
+  );
 }
 
 export default App;
